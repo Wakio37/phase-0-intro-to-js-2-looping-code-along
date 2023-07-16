2f6b241d944f27a569385a8407eeeb94162dfbe2
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-07-16 10:55:06 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+const eventName = "surprise"
+
+function writeCards(names, event) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        const greeting = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        cards.push(greeting);
+      }
+
+      return cards
+    }
+
+    // writeCards(names, eventName)
+
+function countDown(){
+
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown --)
+    }
+}
+
+
+
